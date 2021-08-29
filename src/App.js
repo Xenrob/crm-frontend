ef0b6985db1bce1081437b7a55ecde9e57f95d19
joel.robert <robertjackson38@gmail.com>
2021-08-29 16:47:15 +1100

    layout is added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import { DefaultLayout } from "./layout/DefaultLayout";
 
 
 import { Entry } from "./pages/entry/Entry.page";
@@ -8,7 +9,10 @@ import { Entry } from "./pages/entry/Entry.page";
 function App() {
     return ( 
       <div className = "App" >
-          <Entry />
+          {/* <Entry /> */}
+          <DefaultLayout>
+            //Dashboard
+          </DefaultLayout>
         </div>
     );
 }
